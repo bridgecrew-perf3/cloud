af0767e8ef9f57918ed8bedd2a5b6524c218e1c3
Santosh Kumar <sntshkmr60@gmail.com>
2021-08-31 15:07:50 +0530

    Add sensible default. Overridable.

--- variables.tf
+++ variables.tf
@@ -5,13 +5,16 @@ variable "key_name" {
 
 variable "vpc_region" {
   description = "region for vpc"
+  default = "ap-south-1"
 }
 
 variable "private_subnet_cidr" {
   description = "cidr block for private subnet"
+  default = "10.0.1.0/24"
 }
 
 variable "public_subnet_cidr" {
   description = "cidr block for public subnet"
+  default = "10.0.2.0/24"
 }
 
