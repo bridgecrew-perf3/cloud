e08166b66a6206ff39da3149a271924ab0a1bcb6
Santosh Kumar <sntshkmr60@gmail.com>
2021-09-01 11:16:39 +0530

    Move user data to custom AMI

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "master_node" {
-  ami               = "ami-0bcf5425cdc1d8a85"
+  ami               = "ami-07d2a553c8ada9631"
   instance_type     = "t3a.small"
   availability_zone = var.public_availability_zone
   key_name          = var.key_name
@@ -18,25 +18,6 @@ resource "aws_instance" "master_node" {
     device_index         = 0
     network_interface_id = aws_network_interface.eni-public.id
   }
-
-  user_data = <<EOF
-    #!/bin/bash
-
-    sudo yum update -y -q
-    sudo yum upgrade -y -q
-
-    # Install initial tools
-    sudo yum group install 'Development Tools' -y -q
-    sudo amazon-linux-extras install epel -y
-    sudo yum install vim-X11 golang docker tmux tree python3 git-lfs -y
-
-    # Configure initial tools
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo groupadd docker
-    sudo usermod -aG docker ec2-user
-    sudo newgrp docker
-  EOF
 
   tags = {
     "Name" = var.name_tag
